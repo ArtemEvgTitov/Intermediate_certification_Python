23d773c42de5884dcc60a5c009006dd37e6c915f
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-13 15:13:16 +0300

    Добавлен метод вывода всех заметок в терминал

--- Functions.py
+++ Functions.py
@@ -15,13 +15,25 @@ def load():
         with open(BASE_FILE, "r", encoding="utf-8") as data:
             notes = json.load(data)
     except:
-        print('Создаю новый файл')
+        pass
+
+def print_all():
+    load()
+    print(show_notes(notes))
+
+def show_notes(notes):
+    rez_string = ''
+    for i in notes:
+        for key, value in i.items():
+            rez_string += str(key) + ': ' + str(value) + '\n'
+        rez_string += '\n'
+    return rez_string
 
 def add_note(heading, text):
     load()
     id = search_id()
     data_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    notes.append({"id": id, "data_time": data_time, "heading": heading, "note": text})
+    notes.append({"ID": id, "Дата создания": data_time, "Заголовок": heading, "Заметка": text})
     save()
 
 def search_id():
@@ -30,7 +42,7 @@ def search_id():
     temp = []
     ID = False
     for i in notes:
-        temp.append(i["id"])
+        temp.append(i["ID"])
     while ID == False:
         if id in temp:
             id += 1
