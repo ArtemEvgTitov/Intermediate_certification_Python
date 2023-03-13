9cce5338954150a564f2ed24e0c72e4a137b7ca3
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-13 13:19:28 +0300

    Добавлены Save и Load

--- Functions.py
+++ Functions.py
@@ -1,5 +1,16 @@
 import json
 from datetime import datetime
 
+BASE_FILE = 'notes.json'
 notes = []
+
+def save():
+    with open(BASE_FILE, "w", encoding="utf-8") as note:
+        note.write(json.dumps(notes, ensure_ascii=False))
+    print(f"Заметка успешно сохранена в файле {BASE_FILE}")
+
+def load():
+    global notes
+    with open(BASE_FILE, "r", encoding="utf-8") as data:
+        notes = json.load(data)
 
