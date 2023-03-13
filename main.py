a9f650071e918800e629141e9dfe32ea410b685f
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-13 16:03:59 +0300

    Добавлены ответы на все вопросы. Рекурсия

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ message_level1 = 'Вот какие действия Вам доступны:\n'
                 '4 - Выход\n' \
                 'введите цифру с необходимым действием: '
 
-message_level2 = 'Вот какие действия Вам доступны:\n' \
-                '1 - Выбрать заметку\n' \
-                '2 - Назад\n' \
+message_level2 = 'По какому параметру произвести поиск:\n' \
+                '1 - По ID\n' \
+                '2 - По заголовку\n' \
+                '3 - По тексту заметки\n' \
                 'введите цифру с необходимым действием: '
 
 message_level3 = 'Вот какие действия Вам доступны:\n' \
@@ -26,9 +27,19 @@ def start():
     answer = input(message_level1)
     if answer == '1':
         func.print_all()
-    if answer == "2":
+        start()
+    elif answer == "2":
         heading = input("Введите заголовок для заметки: ")
         text = input("Введите текст заметки: ")
         func.add_note(heading, text)
+        start()
+    elif answer == '3':
+        func.search_note(input(message_level2))
+        start()
+    elif answer == '4':
+        func.stop_programm()
+    else:
+        print("Некорректный ввод")
+        start()
 
 
