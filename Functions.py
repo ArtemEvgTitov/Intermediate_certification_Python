ec4228aa7de40e880a5fcc9072552164a95ea919
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-13 15:00:25 +0300

    Добавлен метод подбора id

--- Functions.py
+++ Functions.py
@@ -10,12 +10,30 @@ def save():
     print(f"Заметка успешно сохранена в файле {BASE_FILE}")
 
 def load():
-    global notes
-    with open(BASE_FILE, "r", encoding="utf-8") as data:
-        notes = json.load(data)
+    try:
+        global notes
+        with open(BASE_FILE, "r", encoding="utf-8") as data:
+            notes = json.load(data)
+    except:
+        print('Создаю новый файл')
 
-def add_note(text):
+def add_note(heading, text):
     load()
+    id = search_id()
     data_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    notes.append({"data_time": data_time, "note": text})
+    notes.append({"id": id, "data_time": data_time, "heading": heading, "note": text})
     save()
+
+def search_id():
+    load()
+    id = 0
+    temp = []
+    ID = False
+    for i in notes:
+        temp.append(i["id"])
+    while ID == False:
+        if id in temp:
+            id += 1
+        else:
+            ID = True
+    return id
