ceb534e73fa75253d3c1ba09b53be69ce82557a5
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-15 08:26:49 +0300

    Дописан метод поиска

--- Functions.py
+++ Functions.py
@@ -8,17 +8,17 @@ notes = []
 def save():
     with open(BASE_FILE, "w", encoding="utf-8") as note:
         note.write(json.dumps(notes, ensure_ascii=False))
-    print(f"Заметка успешно сохранена в файле {BASE_FILE}")
-    log.text_in_log(f"Заметка успешно сохранена в файле {BASE_FILE}")
+    print(f'Заметка успешно сохранена в файле {BASE_FILE}')
+    log.text_in_log(f'Заметка успешно сохранена в файле {BASE_FILE}')
 
 def load():
     try:
         global notes
         with open(BASE_FILE, "r", encoding="utf-8") as data:
             notes = json.load(data)
-        log.text_in_log("База успешно загружена")
+        log.text_in_log('База успешно загружена')
     except:
-        log.text_in_log("Ошибка загрузки базы. Вероятно, отсуствует файл")
+        log.text_in_log('Ошибка загрузки базы. Вероятно, отсуствует файл')
 
 def print_all():
     load()
@@ -27,7 +27,7 @@ def print_all():
         for key, value in i.items():
             result += str(key) + ': ' + str(value) + '\n'
         result += '\n'
-    log.text_in_log("Вывод всех заметок в терминал")
+    log.text_in_log('Вывод всех заметок в терминал')
     print(result)
 
 def add_note(heading, text):
@@ -36,7 +36,7 @@ def add_note(heading, text):
         id = selection_id()
         data_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         notes.append({"ID": id, "Дата создания": data_time, "Заголовок": heading, "Заметка": text})
-        log.text_in_log(f"В базу добавлена заметка: \nID:{id}\nЗаголовок: {heading}\nТекст: {text}")
+        log.text_in_log(f'В базу добавлена заметка: \nID:{id}\nЗаголовок: {heading}\nТекст: {text}')
         save()
     except:
         pass
@@ -65,15 +65,29 @@ def search_note(text):
                 result = ''
                 for key, value in i.items():
                     result += str(key) + ': ' + str(value) + '\n'
-                log.text_in_log(f"Произведён поиск по ID. Результат поиска:\n{result}")
+                log.text_in_log(f'Произведён поиск по ID. Результат поиска:\n{result}')
                 print(result)
-                delete_note(id)
     elif text == '2':
-        heading = input('Введите текст заголовка: ')
-        pass
+        heading = (input('Введите текст заголовка: ')).lower()
+        for i in notes:
+            if heading in i["Заголовок"]:
+                result = ''
+                for key, value in i.items():
+                    result += str(key) + ': ' + str(value) + '\n'
+                log.text_in_log(f'Произведён поиск по ID. Результат поиска:\n{result}')
+                print(result)
     elif text == '3':
-        note = input('Введите текст заметки: ')
-        pass
+        note = (input('Введите текст заметки: ')).lower()
+        for i in notes:
+            if note in i["Заметка"]:
+                result = ''
+                for key, value in i.items():
+                    result += str(key) + ': ' + str(value) + '\n'
+                log.text_in_log(f'Произведён поиск по ID. Результат поиска:\n{result}')
+                print(result)
+
+def search_cycle(notes):
+    pass
 
 def delete_note(id):
     load()
