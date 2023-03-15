48cb8ee5e9591cea7b9b63905212666ef379812c
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-15 15:51:48 +0300

    добавлен edit

--- Functions.py
+++ Functions.py
@@ -39,7 +39,7 @@ def add_note(heading, text):
         load()
         id = selection_id()
         data_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        notes.append({"ID": id, "Дата создания": data_time, "Заголовок": heading, "Заметка": text})
+        notes.append({"ID": id, "Дата создания": data_time, "Дата изменения": data_time, "Заголовок": heading, "Заметка": text)
         log.text_in_log(f'В базу добавлена заметка: \nID:{id}\nЗаголовок: {heading}\nТекст: {text}')
         save()
     except:
@@ -61,14 +61,16 @@ def selection_id():
     log.text_in_log(f'Осуществлён подбор ID для новой заметки. ID:{id}')
     return id
 
-def search_note(text, message):
+def search_note(text):
     load()
     result = ''
+    search_id = 0
     if text == '1':
         id = input('Введите ID заметки: ')
         log.text_in_log(f'Пользователь искал ID: "{id}"')
         for i in notes:
             if int(id) == i["ID"]:
+                search_id = int(i["ID"])
                 for key, value in i.items():
                     result += str(key) + ': ' + str(value) + '\n'
                 log.text_in_log(f'Произведён поиск по ID. Результат поиска:\n{result}')
@@ -77,6 +79,7 @@ def search_note(text, message):
         log.text_in_log(f'Пользователь искал заголовок: "{heading}"')
         for i in notes:
             if heading.lower() in (i["Заголовок"]).lower():
+                search_id = int(i["ID"])
                 for key, value in i.items():
                     result += str(key) + ': ' + str(value) + '\n'
                 log.text_in_log(f'Произведён поиск по заголовку. Результат поиска:\n{result}')
@@ -85,6 +88,7 @@ def search_note(text, message):
         log.text_in_log(f'Пользователь искал текст заметки: "{note}"')
         for i in notes:
             if note.lower() in (i["Заметка"]).lower():
+                search_id = int(i["ID"])
                 for key, value in i.items():
                     result += str(key) + ': ' + str(value) + '\n'
                 log.text_in_log(f'Произведён поиск по ID. Результат поиска:\n{result}')
@@ -92,8 +96,32 @@ def search_note(text, message):
         print('Некорректный ввод. Программа будет перезапущена')
         log.text_in_log(f'Некорректный ввод в меню поиска. Пользователь ввёл "{text}"')
         main.start()
-    return result
+    return result, search_id
 
+def options_for_note(text, id, message_level4):
+    load()
+    if text == '1':
+        edit_note(id, input(message_level4))
+    elif text == '2':
+        delete_note(id)
+
+def edit_note(id, text):
+    load()
+    temp = []
+    for i in notes:
+        if int(id) == i['ID']:
+            temp.append(i)
+    if text == '1':
+        heading = input('Введите новый заголовок: ')
+        temp["Заголовок"] = heading
+    elif text == '2':
+        body = input('Введите новый текст заметки')
+        temp["Заметка"] = body
+    data_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    temp["Дата изменения"] = data_time
+
+def add_edit_note(id):
+    delete_note(id)
 
 def delete_note(id):
     load()
