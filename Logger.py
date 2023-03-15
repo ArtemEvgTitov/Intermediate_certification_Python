a636bb31f91210edfb4922041d65bbcdaf2d8cf1
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-15 17:15:35 +0300

    Добавлены комментарии

--- Logger.py
+++ Logger.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 
 def text_in_log(text):
+    # Логирование
     path = 'log_notes.csv'
     time_sign = dt.now().strftime('%D %H:%M')
     f = open(path, 'a', encoding="utf-8")
